52310a0a1b10ce7ad2e35c45ec2c4841b2ec9646
SukruthaM98 <msukrutha@gmail.com>
2020-10-13 22:50:03 +0530

    verifying mandatory starting part

--- emailverify.sh
+++ emailverify.sh
@@ -2,4 +2,15 @@
 
 echo "welcome to email verification program"
 
+read -p "enter emailid: " emailid
+#verifying mandatory starting part
+emailidpat="^[A-Za-z0-9]+"
+
+if [[ $emailid =~ $emailidpat ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
+
 

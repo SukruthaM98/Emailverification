679307446cdabad3f760a5e19e83559dd043ce24
SukruthaM98 <msukrutha@gmail.com>
2020-10-13 23:15:06 +0530

    validating .xyz in abx.xyz@bridgelabz.co.in

--- emailverify.sh
+++ emailverify.sh
@@ -3,8 +3,8 @@
 echo "welcome to email verification program"
 
 read -p "enter emailid: " emailid
-#verifying mandatory .com .co etc
-emailidpattern="^([A-Za-z0-9]+)?@[a-zA-Z]+.[a-zA-Z]{2,4}"
+#verifying .xyz in abc.xyz@bridgelabz.co.in 
+emailidpattern="^[A-Za-z0-9]+([._+-][0-9a-zA-Z]+)?@[a-zA-Z]+.[a-zA-Z]{2,4}"
 
 if [[ $emailid =~ $emailidpattern ]]
 then
